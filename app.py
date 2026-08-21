716c115a1b3524b7cdfbdbd560b4df7f3a929239
https://github.com/koritala-sai <sailakshmikoritala08@gmail.com>
2026-08-21 11:49:20 +0530

    fixed issue

--- app.py
+++ app.py
@@ -31,11 +31,13 @@ def create_app():
     app.register_blueprint(booking_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(api_bp)
-
+    
+    with app.app_context():
+        db.create_all() 
+        
     return app
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # creates tables in MySQL if they don't exist yet
+     # creates tables in MySQL if they don't exist yet
     app.run(debug=True)
